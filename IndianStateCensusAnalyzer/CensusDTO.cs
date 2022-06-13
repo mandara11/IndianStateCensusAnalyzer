5e85307e038a55a523da123af36462cb052e439c
mandara11 <mandaram2345@gmail.com>
2022-06-13 07:48:38 +0530

    Add project files.

---
+++ IndianStateCensusAnalyzer/CensusDTO.cs
@@ -0,0 +1,20 @@
+﻿using IndianStateCensusAnalyzer.POCO;
+
+namespace IndianStateCensusAnalyzer
+{
+    internal class CensusDTO
+    {
+        private POCO.StateCodeDAO stateCodeDAO;
+        private CensusDataDAO censusDataDAO;
+
+        public CensusDTO(POCO.StateCodeDAO stateCodeDAO)
+        {
+            this.stateCodeDAO=stateCodeDAO;
+        }
+
+        public CensusDTO(CensusDataDAO censusDataDAO)
+        {
+            this.censusDataDAO=censusDataDAO;
+        }
+    }
+}
